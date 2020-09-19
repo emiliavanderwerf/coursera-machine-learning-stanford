142dcc0dc22a58ddb67cbe0d507f5fee1e022935
Emilia Vanderwerf <evanderwerf@catchpoint.com>
2020-09-18 22:05:49 -0400

    Compute sigmoid and cost function for logistic regression

--- assignment2/ex2/costFunction.m
+++ assignment2/ex2/costFunction.m
@@ -7,26 +7,12 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
+% Implement the cost function 
+hypothesis = sigmoid(X * theta);
+summation = sum(-y .* log(hypothesis) - (1 - y) .* log(1 - hypothesis));
+J = (1 / m) * summation;
+
+% Implement gradient descent
 grad = zeros(size(theta));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
-
-
-
-
-
-
-
-
-% =============================================================
 
 end
