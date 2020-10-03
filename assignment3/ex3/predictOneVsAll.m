cd19157c2b3474992b71794d70c667092589f79f
Emilia Vanderwerf <evanderwerf@catchpoint.com>
2020-10-03 14:35:52 -0400

    On new data, predict the digit using the trained classifier in the previous step

--- assignment3/ex3/predictOneVsAll.m
+++ assignment3/ex3/predictOneVsAll.m
@@ -12,7 +12,7 @@ m = size(X, 1);
 num_labels = size(all_theta, 1);
 
 % You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+p = zeros(size(X, 1), 1); % 5000 x 1
 
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
@@ -30,11 +30,9 @@ X = [ones(m, 1) X];
 %       for each row.
 %       
 
-
-
-
-
-
+prediction = sigmoid(X * all_theta');
+[value, index] = max(prediction, [], 2);
+p = index;
 
 % =========================================================================
 
