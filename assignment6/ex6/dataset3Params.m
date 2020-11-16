68e608f4ac1034f05f39ca075eb890d6649ae1b6
Emilia Vanderwerf <emilia.vanderwerf@gmail.com>
2020-11-15 21:10:55 -0500

    Compute the best C and sigma learning parameters for the SVM

--- assignment6/ex6/dataset3Params.m
+++ assignment6/ex6/dataset3Params.m
@@ -23,11 +23,31 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+bestC = 0;
+bestSigma = 0;
+smallestError = 1;
 
+possibleValues = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+for testC = possibleValues
+  for testSigma = possibleValues
+    
+    % Train the model, compute a prediction vector and its error
+    model = svmTrain(X, y, testC, @(x1, x2) gaussianKernel(x1, x2, testSigma));
+    predictions = svmPredict(model, Xval);
+    error = mean(double(predictions ~= yval));
 
+    % If this is the smallest error, save the best C and sigma    
+    if (error < smallestError)
+      smallestError = error;
+      bestC = testC;
+      bestSigma = testSigma;
+    endif
+    
+  endfor
+endfor
 
-
-
+C = bestC;
+sigma = bestSigma;
 
 % =========================================================================
 
