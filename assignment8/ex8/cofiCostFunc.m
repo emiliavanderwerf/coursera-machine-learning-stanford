764f194c1344fba82ab4f0633b0ee8dc650f7a2f
Emilia Vanderwerf <emilia.vanderwerf@gmail.com>
2020-12-28 16:18:55 -0500

    Implement gradient (without regularization) for movie features and movie ratings

--- assignment8/ex8/cofiCostFunc.m
+++ assignment8/ex8/cofiCostFunc.m
@@ -40,8 +40,12 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-
+% Cost function without regularization
 J = (1 / 2) * sum(sum(R .* (X * Theta' - Y).^2));
+
+% Gradient without regularization
+X_grad = (R .* (X * Theta' - Y)) * Theta;
+Theta_grad = (R .* (X * Theta' - Y))' * X;
 
 % =============================================================
 
